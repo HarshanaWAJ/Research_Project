a1deb8ad76a633e4c695adb28d5965e0d9e6539a
Navindya99 <navindyamadhuhansi0@gmail.com>
2025-05-06 18:27:13 +0530

    Initialize video

--- bowler_analyzer.py
+++ bowler_analyzer.py
@@ -184,3 +184,7 @@ class BowlerActionAnalyzer:
         frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         fps = int(cap.get(cv2.CAP_PROP_FPS))
         total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        # Initialize video writer
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
